b42f05c997dc5e7450054003eb05b3a68717cc8c
unknown <komalivutla99@gmail.com>
2023-08-05 12:55:47 +0530

    promotion related changes

---
+++ resources/views/promotion.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Class Promotion</h1>
+
+<form action="/action_page.php">
+  <label for="fname">Category </label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
